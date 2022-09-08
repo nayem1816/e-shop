2da274c8745d341af993bf14ca2405fc888dcd48
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2022-09-08 18:51:24 +0600

    react lezy loading add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import AllProducts from './components/AllProducts/AllProducts';
-import Shirt from './components/Shirts/Shirt';
-import Polo from './components/Polo/Polo';
-import Jacket from './components/Jacket/Jacket';
-import Pant from './components/Pants/Pant';
-import Footer from './components/Footer/Footer';
-import SingleProduct from './components/SingleProduct/SingleProduct';
-import Cart from './components/Cart/Cart';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import Login from './components/Login/Login';
-import SignUp from './components/Login/SignUp';
 import Protected from './components/Login/Protected';
-import Dashboard from './components/Dashboard/Dashboard';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Loading from './components/Loading/Loading';
+
+const Home = React.lazy(() => import('./components/Home/Home'));
+const AllProducts = React.lazy(() =>
+    import('./components/AllProducts/AllProducts')
+);
+const Shirt = React.lazy(() => import('./components/Shirts/Shirt'));
+const Polo = React.lazy(() => import('./components/Polo/Polo'));
+const Jacket = React.lazy(() => import('./components/Jacket/Jacket'));
+const Pant = React.lazy(() => import('./components/Pants/Pant'));
+const SingleProduct = React.lazy(() =>
+    import('./components/SingleProduct/SingleProduct')
+);
+const Cart = React.lazy(() => import('./components/Cart/Cart'));
+const Login = React.lazy(() => import('./components/Login/Login'));
+const SignUp = React.lazy(() => import('./components/Login/SignUp'));
+const Dashboard = React.lazy(() => import('./components/Dashboard/Dashboard'));
 
 function App() {
     return (
@@ -25,29 +31,34 @@ function App() {
             className="main grid grid-cols-1 content-between"
         >
             <div className="">
-                <ToastContainer />
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/allProducts" element={<AllProducts />} />
-                    <Route path="/shirt" element={<Shirt />} />
-                    <Route path="/polo" element={<Polo />} />
-                    <Route path="/jacket" element={<Jacket />} />
-                    <Route path="/pant" element={<Pant />} />
-                    <Route path="/product/:id" element={<SingleProduct />} />
-                    <Route
-                        path="/cart"
-                        element={
-                            <Protected>
-                                <Cart />
-                            </Protected>
-                        }
-                    />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="*" element={<h1>404 Not Found</h1>} />
-                </Routes>
+                <Suspense fallback={<Loading />}>
+                    <ToastContainer />
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/allProducts" element={<AllProducts />} />
+                        <Route path="/shirt" element={<Shirt />} />
+                        <Route path="/polo" element={<Polo />} />
+                        <Route path="/jacket" element={<Jacket />} />
+                        <Route path="/pant" element={<Pant />} />
+                        <Route
+                            path="/product/:id"
+                            element={<SingleProduct />}
+                        />
+                        <Route
+                            path="/cart"
+                            element={
+                                <Protected>
+                                    <Cart />
+                                </Protected>
+                            }
+                        />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="*" element={<h1>404 Not Found</h1>} />
+                    </Routes>
+                </Suspense>
             </div>
             <div className="">
                 <Footer />
